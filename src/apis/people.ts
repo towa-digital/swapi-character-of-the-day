750da585337fe60690a49a43094b68b0f1494d81
Thai Phan <minh-thai.phan@towa.at>
2024-09-11 22:47:57 +0200

    feat: Add apis

---
+++ src/apis/people.ts
@@ -0,0 +1,25 @@
+import { SwapiPeople } from "@types";
+
+const swapiBaseURL = "https://swapi.dev/api";
+
+export const fetchPeople = async ({
+  id,
+}: {
+  id: string;
+}): Promise<SwapiPeople> => {
+  try {
+    const response = await fetch(`${swapiBaseURL}/people/${id}`);
+    return (await response.json()) as SwapiPeople;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchPeopleTotal = async (): Promise<number> => {
+  try {
+    const response = await fetch(`${swapiBaseURL}/people`);
+    return (await response.json()).count;
+  } catch (error) {
+    throw error;
+  }
+};
