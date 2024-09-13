224de59ba0c4c3cb7031581e1db64fd7e05ee770
Thai Phan <minh-thai.phan@towa.at>
2024-09-13 22:15:48 +0200

    refactor: Dropdown

--- src/components/CharacterOfTheDay.tsx
+++ src/components/CharacterOfTheDay.tsx
@@ -71,154 +71,156 @@ export default function CharacterOfTheDay({
   const [colorPants, setColorPants] = useState<Color>(parseColor("#667a8c"));
 
   return (
-    <div className="app-layout p-8 size-full">
+    <>
       <div
         className={clsx(
-          "fixed inset-1 bottom-[35%] lg:bottom-0 bg-cover bg-center z-[-1]",
+          "fixed inset-0 bottom-[35%] lg:bottom-0 bg-cover bg-center z-[-1]",
           Array.from(selectedBg)?.[0] ?? ""
         )}
       />
 
       <div
         className={clsx(
-          "fixed inset-1 top-[30%] lg:top-[50%] lg:from-50% bg-gradient-to-t from-black from-70% to-transparent z-[-1]"
+          "fixed inset-0 top-[30%] lg:top-[50%] lg:from-50% bg-gradient-to-t from-black from-70% to-transparent z-[-1]"
         )}
       />
 
-      <div className="[grid-area:header] gap-4 flex justify-center lg:justify-start items-center flex-col lg:gap-20 lg:flex-row">
-        <Image
-          src="/images/logo.svg"
-          height={28}
-          width={167}
-          alt="StarWars Character of the day Logo"
-        />
+      <div className="app-layout p-8 size-full max-w-screen-lg mx-auto">
+        <div className="[grid-area:header] gap-4 flex justify-center lg:justify-start items-center flex-col lg:gap-20 lg:flex-row">
+          <Image
+            src="/images/logo.svg"
+            height={28}
+            width={167}
+            alt="StarWars Character of the day Logo"
+          />
 
-        <span className="w-full h-px bg-gradient-to-r from-transparent via-primary to to-transparent" />
-      </div>
+          <span className="w-full h-px bg-gradient-to-r from-transparent via-primary to to-transparent" />
+        </div>
 
-      <h1 className="[grid-area:character] lg:[grid-area:title] hyphens-auto text-center text-[6.5rem] [word-break:break-word] text-primary my-auto lg:text-start lg:text-[8.75rem] font-light leading-[0.9] lg:tracking-[-0.6125rem]">
-        {character.name}
-      </h1>
+        <h1 className="[grid-area:character] lg:[grid-area:title] hyphens-auto text-center text-[6.5rem] [word-break:break-word] text-primary my-auto lg:text-start lg:text-[8.75rem] font-light leading-[0.9] lg:tracking-[-0.6125rem]">
+          {character.name}
+        </h1>
 
-      <div className="[grid-area:info] mt-4 lg:mt-0 flex flex-col items-start lg:w-80 gap-1">
-        <Card header="Basic Informations" className="w-full">
-          <ul className="flex flex-col mt-[0.375rem] gap-[0.375rem]">
-            {basicInformations.map((item, index) => (
-              <li
-                key={`basicInformations_${index}`}
-                className="flex text-sm justify-between"
-              >
-                <Text className="tracking-[-0.035rem] opacity-70">
-                  {item.label}
-                </Text>
-                <Text
-                  slot="description"
-                  className={clsx(
-                    "tracking-[-0.0175rem] flex items-center",
-                    item.label === "Gender" && "capitalize"
-                  )}
+        <div className="[grid-area:info] mt-4 lg:mt-0 flex flex-col items-start lg:w-80 gap-1">
+          <Card header="Basic Informations" className="w-full">
+            <ul className="flex flex-col mt-[0.375rem] gap-[0.375rem]">
+              {basicInformations.map((item, index) => (
+                <li
+                  key={`basicInformations_${index}`}
+                  className="flex text-sm justify-between"
                 >
-                  {item.value}
-                  {item.label === "Gender" &&
-                    ["male", "female"].includes(item.value) &&
-                    (item.value === "male" ? (
-                      <GenderMale
-                        width={14}
-                        height={14}
-                        className="text-white ms-1"
-                      />
-                    ) : (
-                      <GenderFemale
-                        width={14}
-                        height={14}
-                        className="text-white ms-1"
-                      />
-                    ))}
-                </Text>
-              </li>
-            ))}
-          </ul>
-        </Card>
+                  <Text className="tracking-[-0.035rem] opacity-70">
+                    {item.label}
+                  </Text>
+                  <Text
+                    slot="description"
+                    className={clsx(
+                      "tracking-[-0.0175rem] flex items-center",
+                      item.label === "Gender" && "capitalize"
+                    )}
+                  >
+                    {item.value}
+                    {item.label === "Gender" &&
+                      ["male", "female"].includes(item.value) &&
+                      (item.value === "male" ? (
+                        <GenderMale
+                          width={14}
+                          height={14}
+                          className="text-white ms-1"
+                        />
+                      ) : (
+                        <GenderFemale
+                          width={14}
+                          height={14}
+                          className="text-white ms-1"
+                        />
+                      ))}
+                  </Text>
+                </li>
+              ))}
+            </ul>
+          </Card>
 
-        <Card header="Appreance" className="w-full">
-          <ul className="flex flex-col mt-[0.375rem] gap-[0.375rem]">
-            {appreance.map((item, index) => (
-              <li
-                key={`appreance_${index}`}
-                className="flex text-sm justify-between"
-              >
-                <Text className="tracking-[-0.035rem] opacity-70">
-                  {item.label}
-                </Text>
-                <Text
-                  slot="description"
-                  className="tracking-[-0.0175rem] capitalize"
+          <Card header="Appreance" className="w-full">
+            <ul className="flex flex-col mt-[0.375rem] gap-[0.375rem]">
+              {appreance.map((item, index) => (
+                <li
+                  key={`appreance_${index}`}
+                  className="flex text-sm justify-between"
                 >
-                  {item.value}
-                </Text>
-              </li>
-            ))}
-          </ul>
-        </Card>
+                  <Text className="tracking-[-0.035rem] opacity-70">
+                    {item.label}
+                  </Text>
+                  <Text
+                    slot="description"
+                    className="tracking-[-0.0175rem] capitalize"
+                  >
+                    {item.value}
+                  </Text>
+                </li>
+              ))}
+            </ul>
+          </Card>
 
-        <Card header="Customize" className="w-full">
-          <Toolbar
-            aria-label="Tools"
-            className="flex gap-[0.625rem] items-center mt-2"
-          >
-            <ColorPicker
-              color={colorShirt}
-              onChange={setColorShirt}
-              label="Shirt"
-            />
-            <ColorPicker
-              color={colorShoes}
-              onChange={setColorShoes}
-              label="Shoes"
-            />
-            <ColorPicker
-              color={colorPants}
-              onChange={setColorPants}
-              label="Pants"
-            />
-          </Toolbar>
-        </Card>
+          <Card header="Customize" className="w-full">
+            <Toolbar
+              aria-label="Tools"
+              className="flex gap-[0.625rem] items-center mt-2"
+            >
+              <ColorPicker
+                color={colorShirt}
+                onChange={setColorShirt}
+                label="Shirt"
+              />
+              <ColorPicker
+                color={colorShoes}
+                onChange={setColorShoes}
+                label="Shoes"
+              />
+              <ColorPicker
+                color={colorPants}
+                onChange={setColorPants}
+                label="Pants"
+              />
+            </Toolbar>
+          </Card>
+        </div>
+
+        <div className="[grid-area:character] flex items-end justify-center">
+          <Character
+            colors={{
+              skin: getColor(character.skin_color),
+              eyes: getColor(character.eye_color),
+              hair: getColor(character.hair_color),
+              shirt: colorShirt.toString(),
+              shoes: colorShoes.toString(),
+              pants: colorPants.toString(),
+            }}
+            gender={character.gender}
+            scaleXFactor={getScaleXFactor(character.height, character.mass)}
+            className="[&>svg]:h-[20rem] [&>svg]:w-[15rem] lg:[&>svg]:h-[40rem] lg:[&>svg]:w-[30rem]"
+          />
+        </div>
+
+        <div className="[grid-area:character] items-end lg:[grid-area:actions] flex gap-3 lg:flex-col justify-end lg:gap-4">
+          <Dropdown
+            label="Change Background"
+            selected={selectedBg}
+            onChange={setSelectedBg}
+            items={backgrounds.map((background) => ({
+              key: background.class,
+              label: background.name,
+            }))}
+            icon={<ImageSquare width={16} height={16} />}
+          />
+
+          <Button
+            label="Re-generate Character"
+            icon={<UserSwitch width={16} height={16} />}
+            onPress={() => refetchCharacter()}
+          />
+        </div>
       </div>
-
-      <div className="[grid-area:character] flex items-end justify-center">
-        <Character
-          colors={{
-            skin: getColor(character.skin_color),
-            eyes: getColor(character.eye_color),
-            hair: getColor(character.hair_color),
-            shirt: colorShirt.toString(),
-            shoes: colorShoes.toString(),
-            pants: colorPants.toString(),
-          }}
-          gender={character.gender}
-          scaleXFactor={getScaleXFactor(character.height, character.mass)}
-          className="[&>svg]:h-[20rem] [&>svg]:w-[15rem] lg:[&>svg]:h-[40rem]"
-        />
-      </div>
-
-      <div className="[grid-area:character] items-end lg:[grid-area:actions] flex gap-3 lg:flex-col justify-end lg:gap-4">
-        <Dropdown
-          label="Change Background"
-          selected={selectedBg}
-          onChange={setSelectedBg}
-          items={backgrounds.map((background) => ({
-            key: background.class,
-            label: background.name,
-          }))}
-          icon={<ImageSquare width={16} height={16} />}
-        />
-
-        <Button
-          label="Re-generate Character"
-          icon={<UserSwitch width={16} height={16} />}
-          onPress={() => refetchCharacter()}
-        />
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,12 +1,17 @@
 import clsx from "clsx";
 import {
+  Collection,
+  Header,
   Menu,
   MenuItem,
   MenuTrigger,
   Popover,
+  Section,
+  Text,
   type Selection,
 } from "react-aria-components";
 import Button from "@/components/ui/Button";
+import CheckCircle from "@/icons/CheckCircle";
 
 export default function Dropdown({
   items,
@@ -25,25 +30,47 @@ export default function Dropdown({
     <MenuTrigger>
       <Button label={label} icon={icon} />
       <Popover>
+        <Text className="lg:sr-only">Select an item</Text>
         <Menu
           items={items}
           selectionMode="single"
           selectedKeys={selected}
           onSelectionChange={onChange}
-          className="flex flex-col gap-2 min-w-44 p-2 bg-neutral-900 backdrop-blur-sm border border-neutral-500 rounded shadow"
+          className="fixed start-0 end-0 bottom-0 lg:static flex flex-col gap-2 min-w-44 py-2 bg-[#2A2A2A] rounded-t-3xl lg:rounded-lg"
         >
-          {(item) => (
-            <MenuItem
-              id={item.key}
-              className={({ isSelected, isHovered }) =>
-                clsx("hover:cursor-pointer px-2 py-1.5", {
-                  "font-bold bg-purple-500 rounded": isSelected || isHovered,
-                })
-              }
-            >
-              {item.label}
-            </MenuItem>
-          )}
+          <Section>
+            <Header className="py-3 text-center mb-2 lg:sr-only">
+              {label}
+            </Header>
+            <Collection items={items}>
+              {(item) => (
+                <MenuItem
+                  id={item.key}
+                  className={({ isSelected, isHovered }) =>
+                    clsx(
+                      "hover:cursor-pointer px-6 py-3 flex items-center lg:text-xs",
+                      {
+                        "bg-white/10": isSelected || isHovered,
+                      }
+                    )
+                  }
+                >
+                  {({ isSelected }) => (
+                    <>
+                      {item.label}
+                      {isSelected && (
+                        <CheckCircle
+                          width={20}
+                          height={20}
+                          className="ms-auto"
+                        />
+                      )}
+                    </>
+                  )}
+                </MenuItem>
+              )}
+            </Collection>
+          </Section>
         </Menu>
       </Popover>
     </MenuTrigger>
