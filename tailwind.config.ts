43ef5f349167c30437188388f63572fd3d54c259
Thai Phan <minh-thai.phan@towa.at>
2024-09-13 15:55:50 +0200

    feat: Add custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,8 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      backgroundImage: {
-        "galaxy": 'url("https://cdn.pixabay.com/animation/2024/07/23/19/52/19-52-56-478_512.gif")',
-        "starships": 'url("https://cdnb.artstation.com/p/assets/images/images/059/819/933/original/massivearmadapx-imperial-fleet-animation.gif?1677195928")',
-        "planet": 'url("https://steamuserimages-a.akamaihd.net/ugc/1856044623170311383/A8A4344688AC7C7C84215339EFB41A28C6A0FF03/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")',
-        "landscape": 'url("https://steamuserimages-a.akamaihd.net/ugc/2004697838472379658/AF0892008D217230690F5916DF99DDADC33EBF73/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true")',
+      fontFamily: {
+        sans: ["var(--font-space-grotesk)"],
       },
     },
   },
